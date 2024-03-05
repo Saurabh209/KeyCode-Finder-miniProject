5dd61bd57a5632391bb5cf1f70e4518220f6630e
Saurabh209 <saurabhhh2613@gmail.com>
2024-03-06 00:52:29 +0530

    Day3 basic update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function onpress(e){
     document.querySelector(".code").innerHTML=`KeyPressed: ${e.key} `;
     document.querySelector(".upperContainer").innerHTML="";
+    console.log(e.target.value);
 
 }
 
