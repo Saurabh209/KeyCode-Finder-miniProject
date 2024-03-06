74866d98026c87cee56b1fcd77215d927cac029e
Saurabh209 <saurabhhh2613@gmail.com>
2024-03-06 16:28:04 +0530

    new functionality added

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const Key=document.querySelector("#event.key");
+const code=document.querySelector("#eventcode");
+const Which=document.querySelector("#event.which");
+const more=document.querySelector("#event.more");
+// const Key=document.querySelector("#event.key");
+// const Key=document.querySelector("#event.key");
 
 
 function onpress(e){
-    const newCode=document.querySelector(".upperContainer").innerHTML=`<h2>KeyPressed: ${e.key} </h2><br><p>${e.keyCode}`;
-    console.log(e.target.value);
+    const newCode=document.querySelector(".upperContainer").innerHTML=`<h2>KeyPressed: ${e.key} </h2><br><h1 style="font-size:90px">${e.which}</h1><br><h5> The keyCode`;    
+
 
 }
 
