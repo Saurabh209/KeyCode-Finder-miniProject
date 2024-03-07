c8fb58b59eaf19bec9cd2a9659bf4287984dd275
Saurabh209 <saurabhhh2613@gmail.com>
2024-03-07 15:52:41 +0530

    new file added

---
+++ script2.js
@@ -0,0 +1,64 @@
+function onpress(e){
+    const data=document.querySelector(".afterContainer");
+    data.innerHTML=`<div class="upperContainer">
+    <h2>KeyPressed: ${e.key} </h2><br><h1 style="font-size:90px">${e.which}</h1><br><h5> The keyCode
+  </div>
+  <hr><br><br>
+  <div class="lowerContainer">
+    <div class="data">
+      <div class="title">
+        <p>event.key</p>
+      </div>
+      <div class="variable" id="eventKey">
+        <P>${e.key}</P>
+      </div>
+      <div class="card-info">
+        <p><small>This property shows key that was pressed during a keyboard event.</small></p>
+      </div>
+    </div>
+    <div class="data">
+      <div class="title">
+        <p>event.code</p>
+      </div>
+      <div class="variable" id="eventCode">
+        <div class="variable-data">
+          <p>${e.code}</p>
+        </div>
+
+      </div>
+      <div class="card-info">
+        <p><small>This property provides information about the physical key code associated with a keyboard event</small></p>
+      </div>
+    </div>
+    <div class="data">
+      <div class="title">
+        <p>More</p>
+      </div>
+      <div class="variable" id="more">
+        <div class="variable-data">
+          <P>---</P>
+        </div>
+      </div>
+      <div class="card-info">
+
+      </div>
+    </div>
+    <div class="data">
+      <div class="title">
+        <p>box4</p>
+      </div>
+      <div class="variable">
+        <div class="variable-data">
+          <p>---</p>
+        </div>
+
+      </div>
+      <div class="card-info">
+
+      </div>
+    </div>
+  </div>  
+`
+};
+
+window.addEventListener('keypress',onpress);
